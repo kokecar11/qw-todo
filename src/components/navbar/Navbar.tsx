1dfaec2c73723f7980a0115d6c0275f0fd70b288
Koke Carpintero <kokecar11.dev@gmail.com>
2023-05-06 19:01:51 -0500

    fix: added file css navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,26 +1,11 @@
-import { Slot, component$, useStylesScoped$ } from "@builder.io/qwik";
+import { Slot, component$, useStyles$ } from "@builder.io/qwik";
+import styleNavbar from './Navbar.css?inline'
 
 
 export const Navbar = component$(() => {
-    useStylesScoped$(`
-    .nav-link {
-        @apply text-primary-500 p-5 font-semibold transition duration-200 ease-in-out
-        hover:bg-primary-500 hover:bg-opacity-10 hover:border-primary-500 hover:border-b-2
-        dark:hover:bg-white dark:hover:bg-opacity-10 dark:hover:border-white dark:hover:border-b-2 dark:text-white
-      }
-        
-      .active-nav-item {
-        @apply border-b-2 bg-opacity-20 bg-primary-500 border-primary-500
-        dark:border-secondary-500 dark:text-white dark:bg-white dark:bg-opacity-20
-      }
-      
-      .navbar {
-          @apply sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 border-b border-primary-500 border-opacity-20
-          dark:bg-primary-900 dark:border-secondary-500 dark:border-opacity-20 dark:backdrop-blur-lg dark:bg-opacity-20
-      }
-  `);
+    useStyles$(styleNavbar);
     return (
-        <nav class={"navbar"}>
+        <nav class="navbar">
             <div class="mx-2 sm:mx-6 lg:mx-40">
                 <div class="relative flex h-16 items-center justify-between">
                     <div class="flex flex-1 sm:items-stretch sm:justify-start">

cd152134574efc2a3ccc535198bd9905e23d43e9
Koke Carpintero <kokecar11.dev@gmail.com>
2023-05-06 19:00:10 -0500

    fix: change colors in footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,25 +1,29 @@
 import { component$ } from '@builder.io/qwik';
 import { Link } from '@builder.io/qwik-city';
+import { MdiGithub, MdiInstagram, MdiTwitter } from '../icons/icons';
 // import { GlobalStore } from '~/core/context';
 
 
 export const Footer = component$( () => {
     // const state = useContext(GlobalStore);
     return ( 
-        <footer class="bg-secondary-100 text-center text-white dark:bg-primary-900">
+        <footer class="bg-primary-100 dark:bg-primary-900 text-center text-white ">
             <div class="pt-6">
-                <div class="mb-6 flex justify-center">
-                    <Link href="#!" class="icon">
-
+                <div class="mb-6 flex justify-center space-x-6">
+                    <Link href="https://github.com/kokecar11" class="icon">
+                        <MdiGithub class={"text-2xl text-primary-900"} />
                     </Link>
-                    <Link href="#!" class="icon">
-
+                    <Link href="https://twitter.com/Kokecar11" class="icon" >
+                        <MdiTwitter class={"text-2xl text-primary-900"} />
+                    </Link>
+                    <Link href="https://www.instagram.com/koke_car11/" class="icon">
+                        <MdiInstagram class={"text-2xl text-primary-900"} />
                     </Link>
                 </div>
             </div>
             <div
-                class="bg-secondary-100 text-primary-800 dark:bg-primary-900 dark:text-white p-1 text-center">
-                Hecho con el 💙 - <span class={"font-bold text-primary-800"}>Kokecar11</span>
+                class="bg-primary-100 text-primary-900 dark:bg-primary-900 dark:text-white p-1 text-center font-bold">
+                Hecho con el 💙 - Kokecar11
             </div>
         </footer>
     );
